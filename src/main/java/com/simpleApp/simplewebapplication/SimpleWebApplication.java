42e2cdbcb0a32dea050f740bf9d9826001f89df3
steczigo <tradeuruk@gmail.com>
2021-10-15 15:59:31 +0200

    initial commit

---
+++ src/main/java/com/simpleApp/simplewebapplication/SimpleWebApplication.java
@@ -0,0 +1,29 @@
+package com.simpleApp.simplewebapplication;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class SimpleWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleWebApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext context) {
+		return args -> {
+			System.out.println("Let's inspect the beans provided by Spring boot");
+			String[] beanNames = context.getBeanDefinitionNames();
+			Arrays.sort(beanNames);
+			for(String beanName : beanNames) {
+				System.out.println(beanName);
+			}
+		};
+	}
+}
